bbbe60e6ede22f1f3269200912a04e5090a26de0
Artur Tarasau <tarasov.a.web@gmail.com>
2023-04-12 14:55:53 +0300

    complite carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
-
+const LOG_TWO = 0.693;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+  const num = 0.693;
+  if(typeof sampleActivity !== 'string' || isNaN(+sampleActivity) || sampleActivity <= 0 || sampleActivity>MODERN_ACTIVITY){
+    return false
+  }else{
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (num / HALF_LIFE_PERIOD));
+  }
 }
 
 module.exports = {
