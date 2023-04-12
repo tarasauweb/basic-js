36d052a29a14dbe33a74658c0d5f61ae06a56541
Artur Tarasau <tarasov.a.web@gmail.com>
2023-04-12 14:32:49 +0300

    complite count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats( matrix ) {
+ let res= 0;
+ matrix.forEach(item=>{
+  item.forEach(i=>{
+      if(i === '^^'){
+        res++
+      }
+  })
+ })
+ return res
 }
 
 module.exports = {
