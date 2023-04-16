f79e87440cef879e1dafaa52c6f5992800ac8470
Artur Tarasau <tarasov.a.web@gmail.com>
2023-04-16 12:59:42 +0300

    extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+  const myOptions = options;
+  if(!Object.keys(myOptions).includes('separator')){
+    myOptions['separator'] = '+'
+  }
+  if(!Object.keys(myOptions).includes('additionSeparator')){
+    myOptions['additionSeparator'] = '|'
+  }
+  if(!Object.keys(myOptions).includes('repeatTimes')){
+    myOptions['repeatTimes'] = 1
+  }
+  if(!Object.keys(myOptions).includes('addition')){
+    myOptions['addition'] = ''
+  }
+  if(!Object.keys(myOptions).includes('additionRepeatTimes')){
+    myOptions['additionRepeatTimes'] = 1
+  }
+  
+  let arrRes = [];
+  let myStr = str;
+
+  let someStr = (myOptions.addition+myOptions.additionSeparator).repeat(myOptions.additionRepeatTimes || 1)
+  someStr = someStr.slice(0,-myOptions['additionSeparator'].length);
+  myStr+=someStr
+  for(let i = 0 ; i<myOptions.repeatTimes;i++){
+    arrRes.push(myStr+myOptions.separator)
+  }
+  arrRes = arrRes.join('').slice(0,-myOptions['separator'].length);
+  console.log(arrRes)
+  return arrRes
 }
 
 module.exports = {
